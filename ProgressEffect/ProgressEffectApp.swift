4f0e01671bf0a81065c3030b0be383f4baa0a142
baohoang <hoangquangbao.developer@gmail.com>
2023-10-14 21:31:23 +0700

    Init project

---
+++ ProgressEffect/ProgressEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressEffectApp.swift
+//  ProgressEffect
+//
+//  Created by Bao Hoang on 14/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProgressEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
